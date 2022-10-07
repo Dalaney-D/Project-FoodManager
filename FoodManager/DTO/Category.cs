13b5042ea43c85b2745f5fb3cc403af4c1b2fd5b
duynvse151394 <duynvse151394@fpt.edu.vn>
2022-10-07 21:06:27 +0700

    update 1st

---
+++ FoodManager/DTO/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Category
+    {
+        public int CateID { set; get; }
+        public string CateName { set; get; }
+    }
+}
---
+++ FoodManager/DTO/Table.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class Table
+    {
+        public int TableId { set; get; }
+        public int Status { set; get; }
+    }
+}
