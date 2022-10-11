ec5fb520e9cc65581a2b1d5eb49cdec8e3f9f62d
duynvse151394 <duynvse151394@fpt.edu.vn>
2022-10-11 16:04:39 +0700

    edit form

--- FoodManager/Views/Login.Designer.cs
+++ FoodManager/Views/Login.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace FoodManager.Views
+namespace FoodManager
 {
     partial class Login
     {
@@ -29,12 +29,33 @@ namespace FoodManager.Views
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
--- FoodManager/Views/Login.cs
+++ FoodManager/Views/Login.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FoodManager.Views
+namespace FoodManager
 {
     public partial class Login : Form
     {
